51e182342827bdf525148d2166e2248fd75be966
Gastello <maximgst@gmail.com>
2025-06-18 13:23:50 +0300

    pnpm com

--- src/pages/projects/routingPractice.tsx
+++ src/pages/projects/routingPractice.tsx
@@ -2,7 +2,7 @@ import Topic from "../../components/topic/Topic";
 
 export default function RoutingPractice() {
   return (
-    <Topic title="Routing Practice">
+    <Topic title="Project: Routing Practice">
       Проєкт тут:{" "}
       <a
         target="_blank"
